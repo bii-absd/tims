0f09f62954da08643789c5e182257fcc70e0fd49
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-10-30 17:03:52 +0800

    30Oct - Added one new function getAvailable.

--- src/java/Clinical/Data/Sink/Database/SubmittedJob.java
+++ src/java/Clinical/Data/Sink/Database/SubmittedJob.java
@@ -4,6 +4,8 @@
 package Clinical.Data.Sink.Database;
 
 import Clinical.Data.Sink.Bean.AuthenticationBean;
+import Clinical.Data.Sink.General.Constants;
+import java.io.Serializable;
 
 /**
  * SubmittedJob is used to represent the submitted_job table in the database.
@@ -20,8 +22,9 @@ import Clinical.Data.Sink.Bean.AuthenticationBean;
  * 07-Oct-2015 - Added Log4j2 for this class.
  * 12-Oct-2015 - Added job_id field.
  * 23-Oct-2015 - Added report field.
+ * 30-Oct-2015 - Added one new function getAvailable.
  */
-public class SubmittedJob {
+public class SubmittedJob implements Serializable {
     private int job_id;
     private String user_id;
     private int status_id;
@@ -46,6 +49,18 @@ public class SubmittedJob {
     // Return the job status name of this submitted job.
     public String getStatus_name() {
         return JobStatus.getStatusName(status_id);
+    }
+    
+    // Based on the job status, the download link at jobstatus.xhtml will be
+    // enabled or disabled accordingly.
+    public String getAvailable() {
+        if (JobStatus.getStatusName(status_id).compareTo("Completed") == 0)
+        {
+            return Constants.FALSE;
+        }
+        else {
+            return Constants.TRUE;
+        }
     }
     
     // Machine generated setters
