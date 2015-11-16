ed525cf1abc7e02ea8687213692eb43ef7f573ab
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-11-16 15:27:46 +0800

    16Nov - To allow user to create new pipeline command.

--- src/java/Clinical/Data/Sink/Bean/CommandManagementBean.java
+++ src/java/Clinical/Data/Sink/Bean/CommandManagementBean.java
@@ -5,6 +5,7 @@ package Clinical.Data.Sink.Bean;
 
 import Clinical.Data.Sink.Database.PipelineCommand;
 import Clinical.Data.Sink.Database.PipelineCommandDB;
+import Clinical.Data.Sink.General.Constants;
 import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.List;
@@ -27,6 +28,8 @@ import org.primefaces.event.RowEditEvent;
  * Revision History
  * 06-Nov-2015 - Created with all the standard getters and setters. Added
  * method onRowEdit to handle pipeline command updating.
+ * 16-Nov-2015 - Added one new method createNewPipelineCommand, to allow user
+ * to create new pipeline command.
  */
 
 @ManagedBean (name="cmdMgntBean")
@@ -35,6 +38,7 @@ public class CommandManagementBean implements Serializable {
     // Get the logger for Log4j
     private final static Logger logger = LogManager.
             getLogger(CommandManagementBean.class.getName());
+    private String command_id, command_code, command_para;
     private List<PipelineCommand> cmdList;
     
     public CommandManagementBean() {
@@ -46,7 +50,7 @@ public class CommandManagementBean implements Serializable {
     @PostConstruct
     public void init() {
         try {
-            cmdList = PipelineCommandDB.getAllCommand();            
+            cmdList = PipelineCommandDB.getAllPipelineCommand();            
         }
         catch (SQLException e) {
             logger.error("SQLException when trying to query the pipeline commands.");
@@ -57,9 +61,32 @@ public class CommandManagementBean implements Serializable {
         }
     }
 
+    // Create the new pipeline command.
+    public String createNewPipelineCommand() {
+       FacesContext fc = getFacesContext();
+       PipelineCommand newCmd = new PipelineCommand(command_id, command_code,
+                                    command_para);
+       
+       if (PipelineCommandDB.insertPipelineCommand(newCmd)) {
+           logger.info(AuthenticationBean.getUserName() + 
+                   ": created new Pipeline Command: " + command_id);
+           fc.addMessage(null, new FacesMessage(
+                    FacesMessage.SEVERITY_INFO,
+                    "New pipeline command created.", ""));
+       }
+       else {
+           logger.error("Failed to create new pipeline command: " + command_id);
+           fc.addMessage(null, new FacesMessage(
+                    FacesMessage.SEVERITY_ERROR,
+                    "Failed to create new pipeline command!", ""));
+       }
+       
+       return Constants.PIPELINE_COMMAND_MANAGEMENT;
+    }
+    
     // Update the pipeline command in the database too.
     public void onRowEdit(RowEditEvent event) {
-        if (PipelineCommandDB.updateCommand(
+        if (PipelineCommandDB.updatePipelineCommand(
                 (PipelineCommand) event.getObject())) {
             logger.info(AuthenticationBean.getUserName() + 
                     ": updated pipeline command " + 
@@ -80,8 +107,27 @@ public class CommandManagementBean implements Serializable {
     private FacesContext getFacesContext() {
 	return FacesContext.getCurrentInstance();
     }
+    
 
     // Machine generated getters and setters
+    public String getCommand_id() {
+        return command_id;
+    }
+    public void setCommand_id(String command_id) {
+        this.command_id = command_id;
+    }
+    public String getCommand_code() {
+        return command_code;
+    }
+    public void setCommand_code(String command_code) {
+        this.command_code = command_code;
+    }
+    public String getCommand_para() {
+        return command_para;
+    }
+    public void setCommand_para(String command_para) {
+        this.command_para = command_para;
+    }
     public List<PipelineCommand> getCmdList() {
         return cmdList;
     }
