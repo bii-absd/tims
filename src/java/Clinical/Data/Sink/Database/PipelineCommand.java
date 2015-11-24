3d40a30369e95473dcbc35f22a2626e7e1726380
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-11-24 11:07:27 +0800

    24Nov - Changed variable name, and added one variable tid.

--- src/java/Clinical/Data/Sink/Database/PipelineCommand.java
+++ src/java/Clinical/Data/Sink/Database/PipelineCommand.java
@@ -15,33 +15,43 @@ import java.io.Serializable;
  * Revision History
  * 05-Nov-2015 - Created with all the standard getters and setters. Added one 
  * method toString.
+ * 24-Nov-2015 - Changed variable name from command_id to pipeline_name. Added
+ * one variable tid (Technology ID).
  */
 
 public class PipelineCommand implements Serializable {
-    private String command_id;
+    private String pipeline_name;
+    private String tid;
     private String command_code;
     private String command_para;
 
     // Machine generated constructor
-    public PipelineCommand(String command_id, String command_code, 
-            String command_para) {
-        this.command_id = command_id;
+    public PipelineCommand(String pipeline_name, String tid, 
+            String command_code, String command_para) {
+        this.pipeline_name = pipeline_name;
+        this.tid = tid;
         this.command_code = command_code;
         this.command_para = command_para;
     }
 
     // Return the String representation of the pipeline command 
-    // i.e. command_id - command parameter
+    // i.e. pipeline_name - command parameter
     public String toString() {
-        return command_id + " - " + command_code + " " + command_para;
+        return pipeline_name + " - " + command_code + " " + command_para;
     }
     
     // Machine generated getters and setters
-    public String getCommand_id() {
-        return command_id;
+    public String getPipeline_name() {
+        return pipeline_name;
     }
-    public void setCommand_id(String command_id) {
-        this.command_id = command_id;
+    public void setPipeline_name(String pipeline_name) {
+        this.pipeline_name = pipeline_name;
+    }
+    public String getTid() {
+        return tid;
+    }
+    public void setTid(String tid) {
+        this.tid = tid;
     }
     public String getCommand_code() {
         return command_code;
