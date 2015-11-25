94075c4023b7e83721dbb07311512027672b1e13
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-11-25 15:07:50 +0800

    25Nov - Commented out unused code. Implementation for database 2.0

--- src/java/Clinical/Data/Sink/Database/SubmittedJobDB.java
+++ src/java/Clinical/Data/Sink/Database/SubmittedJobDB.java
@@ -3,8 +3,6 @@
  */
 package Clinical.Data.Sink.Database;
 
-import Clinical.Data.Sink.Bean.AuthenticationBean;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -37,6 +35,7 @@ import org.apache.logging.log4j.LogManager;
  * 08-Oct-2015 - Add methods to update the status_id of submitted job.
  * 12-Oct-2015 - Added job_id field during query. Log the exception message.
  * 23-Oct-2015 - Added report field during query.
+ * 25-Nov-2015 - Commented out unused code. Implementation for database 2.0
  */
 
 public class SubmittedJobDB {
@@ -45,9 +44,11 @@ public class SubmittedJobDB {
             getLogger(SubmittedJobDB.class.getName());
     private final static Connection conn = DBHelper.getDBConn();
     private static Map<Integer,SubmittedJob> submittedJobs = new HashMap<>();
+    /* No longer in use
     // By default, the query will be order by job_id DESC
     private static String queryOrderBy = "job_id";
     private static String orderIn = "DESC";
+    */
     
     SubmittedJobDB() {}
     
@@ -57,17 +58,20 @@ public class SubmittedJobDB {
     // handled by the caller.
     public static int insertJob(SubmittedJob job) throws SQLException {
         String insertStr = "INSERT INTO submitted_job"
-                + "(user_id, status_id, study_id, submit_time, output_file, report) "
-                + "VALUES(?,?,?,?,?,?)";
+                + "(study_id, user_id, tid, pipeline_name, status_id, "
+                + "submit_time, output_file, report) "
+                + "VALUES(?,?,?,?,?,?,?,?)";
         PreparedStatement insertStm = conn.prepareStatement(insertStr);
         // Build the INSERT statement using the variables retrieved from the
         // SubmittedJob object (i.e. job) passed in.
-        insertStm.setString(1, job.getUser_id());
-        insertStm.setInt(2, job.getStatus_id());
-        insertStm.setString(3, job.getStudy_id());
-        insertStm.setString(4, job.getSubmit_time());
-        insertStm.setString(5, job.getOutput_file());
-        insertStm.setString(6, job.getReport());
+        insertStm.setString(1, job.getStudy_id());
+        insertStm.setString(2, job.getUser_id());
+        insertStm.setString(3, job.getTid());
+        insertStm.setString(4, job.getPipeline_name());
+        insertStm.setInt(5, job.getStatus_id());
+        insertStm.setString(6, job.getSubmit_time());
+        insertStm.setString(7, job.getOutput_file());
+        insertStm.setString(8, job.getReport());
         // Execute the INSERT statement
         insertStm.executeUpdate();
         // Retrieve and store the last inserted Job ID
@@ -98,18 +102,20 @@ public class SubmittedJobDB {
     }
     
     // The following 3 functions will update the status_id of the submitted_job
-    // KIV: Can we do this without hardcoding?
     public static void updateJobStatusToInprogress(int job_id) {
         updateJobStatus(job_id, 2);
     }    
     public static void updateJobStatusToCompleted(int job_id) {
         updateJobStatus(job_id, 3);
     }
-    public static void updateJobStatusToFailed(int job_id) {
+    public static void updateJobStatusToFinalized(int job_id) {
         updateJobStatus(job_id, 4);
     }
+    public static void updateJobStatusToFailed(int job_id) {
+        updateJobStatus(job_id, 5);
+    }
     
-    // updateJobStatus is the internal helper function for updateJobStatusToXXX
+    // Internal helper function for updateJobStatusToXXX
     // It will update the status_id of the job according to the status_id 
     // passed in.
     private static void updateJobStatus(int job_id, int status_id) {
@@ -120,33 +126,33 @@ public class SubmittedJobDB {
         try (PreparedStatement updateStatus = conn.prepareStatement(updateStr))
         {
             updateStatus.executeUpdate();
-            logger.debug("updateJobStatus success.");
+            logger.debug("Updated job status.");
         }
         catch (SQLException e) {
-            logger.error("SQLException at updateJobStatus.");
+            logger.error("SQLException when updating job status.");
             logger.error(e.getMessage());
         }
     }
     
-    // querySubmittedJob will query the submitted_job table using the user_id 
-    // as a match condition. The results from the query will be returned as
-    // a list to the caller.
+    // Query the submitted_job table using the user_id as a match condition. 
+    // The results from the query will be returned as a list to the caller.
     public static List<SubmittedJob> querySubmittedJob(String user_id) {
         // Only execute the query if the list is empty
         // This is to prevent the query from being run multiple times.
         if (submittedJobs.isEmpty()) {
             ResultSet result;
         
-            String queryStr = "SELECT job_id, status_id, submit_time, "
-                    + "output_file, report, study_id FROM submitted_job WHERE "
-                    + "user_id = ? ORDER BY " + queryOrderBy + " " + orderIn;
-        
-//            logger.debug(queryStr);
-        // Additional logging to get the logger context
-//        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-//        Configuration cfg = ctx.getConfiguration();
-//        FileAppender fa = (FileAppender) cfg.getAppender("File");
-                
+            String queryStr = "SELECT job_id, study_id, tid, pipeline_name, "
+                    + "status_id, submit_time, output_file, report FROM "
+                    + "submitted_job WHERE user_id = ? ORDER BY job_id DESC"; 
+
+            // Additional logging to get the logger context
+            /*
+            LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+            Configuration cfg = ctx.getConfiguration();
+            FileAppender fa = (FileAppender) cfg.getAppender("File");
+            */
+            
             try (PreparedStatement queryJob = conn.prepareStatement(queryStr))
             {
                 // Set the query condition using the user_id passed in.
@@ -156,11 +162,13 @@ public class SubmittedJobDB {
             
                 while (result.next()) {
                     // Create a new SubmittedJob object for every row of data 
-                    // retrieved from the data.
+                    // retrieved from the database.
                     SubmittedJob job = new SubmittedJob(
                                     result.getInt("job_id"),
-                                    result.getInt("status_id"),
                                     result.getString("study_id"),
+                                    result.getString("tid"),
+                                    result.getString("pipeline_name"),
+                                    result.getInt("status_id"),
                                     result.getString("submit_time"),
                                     result.getString("output_file"),
                                     result.getString("report"));
@@ -169,7 +177,7 @@ public class SubmittedJobDB {
                 }
                 logger.debug("querySubmittedJob success.");
             } catch (SQLException e) {
-                logger.error("SQLException at querySubmittedJob.");
+                logger.error("SQLException when query submitted job!");
                 logger.error(e.getMessage());
                 // Exception has occurred, return back a empty list.
                 return new ArrayList<>(0);
@@ -179,6 +187,19 @@ public class SubmittedJobDB {
         return new ArrayList<>(submittedJobs.values());
     }
 
+    // Clear the HashMap, so that the query to the database will be run again.
+    private static void clearSubmittedJobs() {
+        submittedJobs.clear();
+    }
+    
+    /* No longer in use
+    // Machine generated getters and setters
+    public static String getQueryOrderBy() { return queryOrderBy; }
+    public static String getOrderIn() { return orderIn; }
+    public static void setOrderIn(String orderIn) {
+        SubmittedJobDB.orderIn = orderIn;
+    }
+    
     // setQueryOrderBy will sort the result return by query according to the
     // parameter passed in. The order (e.g. ASC/DESC) will be based on the
     // order by type.
@@ -207,16 +228,5 @@ public class SubmittedJobDB {
         // Clear away the result of the last query operation
         clearSubmittedJobs();
     }
-    
-    // clearSubmittedJobs will clear the HashMap, so that the query to the database
-    // will be run again.
-    private static void clearSubmittedJobs() {
-        submittedJobs.clear();
-    }
-    // Machine generated getters and setters
-    public static String getQueryOrderBy() { return queryOrderBy; }
-    public static String getOrderIn() { return orderIn; }
-    public static void setOrderIn(String orderIn) {
-        SubmittedJobDB.orderIn = orderIn;
-    }
+    */
 }
