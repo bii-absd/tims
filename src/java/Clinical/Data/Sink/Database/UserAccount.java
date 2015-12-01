2d856c5f60783a080812e6c42ea89ebe97311428
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-12-01 10:38:51 +0800

    01Dec - Implementation for database 2.0

--- src/java/Clinical/Data/Sink/Database/UserAccount.java
+++ src/java/Clinical/Data/Sink/Database/UserAccount.java
@@ -15,13 +15,14 @@ package Clinical.Data.Sink.Database;
  * 02-Oct-2015 - Added in the comments for the code.
  * 07-Oct-2015 - Added Log4j2 for this class. Changed to connection based for
  * database access.
- * 09-Oct-2015 - Update the class to handle the additional 2 fields (institution
- * and department) in user_account table. Moved the two query methods to the
+ * 09-Oct-2015 - Update the class to handle the additional 2 fields (inst_id
+ * and dept_id) in user_account table. Moved the two query methods to the
  * new class UserAccountDB.
  * 09-Nov-2015 - Added one variable "last_login" to this class. Added two 
  * methods getActiveString() and getRoleString() that return the active status
  * and role ID in wording.
  * 13-Nov-2015 - Removed the Log4j2 and SQL libraries from this class.
+ * 01-Dec-2015 - Implementation for database 2.0
  */
 
 public class UserAccount {
@@ -29,11 +30,11 @@ public class UserAccount {
     private String user_id, first_name, last_name, email, pwd;
     private Boolean active;
     private int role_id;
-    private String department, institution, last_login;
+    private String dept_id, inst_id, last_login;
 
     public UserAccount(String user_id, int role_id, String first_name, 
                        String last_name, String email, Boolean active, 
-                       String pwd, String department, String institution,
+                       String pwd, String dept_id, String inst_id,
                        String last_login) {
         this.user_id = user_id;
         this.role_id = role_id;
@@ -42,8 +43,8 @@ public class UserAccount {
         this.email = email;
         this.active = active;
         this.pwd = pwd;
-        this.department = department;
-        this.institution = institution;
+        this.dept_id = dept_id;
+        this.inst_id = inst_id;
         this.last_login = last_login;
     }
 
@@ -57,23 +58,23 @@ public class UserAccount {
     public void setActive(Boolean active) { this.active = active; }
     public void setPwd(String pwd) { this.pwd = pwd; }
     public void setRole_id(int role_id) { this.role_id = role_id; }
-    public void setDepartment(String department) {
-        this.department = department;
+    public void setDept_id(String dept_id) {
+        this.dept_id = dept_id;
     }
-    public void setInstitution(String institution) {
-        this.institution = institution;
+    public void setInst_id(String inst_id) {
+        this.inst_id = inst_id;
     }
     public void setLast_login(String last_login) {
         this.last_login = last_login;
     }
     
-    // Return the active status in wording
-    public String getActiveString() {
+    // Return the Active status.
+    public String getActiveStatus() {
         return active?"Enabled":"Disabled";
     }
-    // Return the Role in wording
-    public String getRoleString() {
-        return UserRoleDB.getRoleFromHash(role_id);
+    // Return the Role Name.
+    public String getRoleName() {
+        return UserRoleDB.getRoleNameFromHash(role_id);
     }
     
     // Machine generated getters
@@ -84,7 +85,7 @@ public class UserAccount {
     public Boolean getActive() { return active; }
     public String getPwd() { return pwd; }
     public int getRole_id() { return role_id; }
-    public String getDepartment() { return department; }
-    public String getInstitution() { return institution; }
+    public String getDept_id() { return dept_id; }
+    public String getInst_id() { return inst_id; }
     public String getLast_login() { return last_login; }
 }
