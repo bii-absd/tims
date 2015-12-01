af0ba94e84d742f6ba8a3d52e9c52ad0c08679b6
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-12-01 11:29:58 +0800

    01Dec - Implementation for database 2.0

--- src/java/Clinical/Data/Sink/Database/UserAccountDB.java
+++ src/java/Clinical/Data/Sink/Database/UserAccountDB.java
@@ -39,6 +39,7 @@ import org.mindrot.jbcrypt.BCrypt;
  * 2. updateAccount(UserAccount user)
  * 13-Nov-2015 - Added one new variable userIDList and one new method 
  * getAllUserID(). Changed getAllUser() method to build up the userIDList.
+ * 30-Nov-2015 - Implementation for database 2.0
  */
 
 public class UserAccountDB {
@@ -66,27 +67,24 @@ public class UserAccountDB {
     public static List<UserAccount> getAllUser() {
         // Empty the current user list
         userList.clear();
-        String queryStr = "SELECT user_id, first_name, last_name, email, "
-                + "role_id, institution, department, active, last_login FROM "
-                + "user_account ORDER BY user_id";
+        String queryStr = "SELECT * FROM user_account u NATURAL JOIN dept d "
+                + "WHERE u.dept_id = d.dept_id ORDER BY u.user_id";
+        ResultSet result = DBHelper.runQuery(queryStr);
         
-        try (PreparedStatement queryStm = conn.prepareStatement(queryStr)) {
-            ResultSet queryResult = queryStm.executeQuery();
-            String user_id = null;
-            
-            while (queryResult.next()) {
-                user_id = queryResult.getString("user_id");
+        try {
+            while (result.next()) {
+                String user_id = result.getString("user_id");
                 UserAccount user = new UserAccount(
                                         user_id,
-                                        queryResult.getInt("role_id"),
-                                        queryResult.getString("first_name"),
-                                        queryResult.getString("last_name"),
-                                        queryResult.getString("email"),
-                                        queryResult.getBoolean("active"),
+                                        result.getInt("role_id"),
+                                        result.getString("first_name"),
+                                        result.getString("last_name"),
+                                        result.getString("email"),
+                                        result.getBoolean("active"),
                                         "password",
-                                        queryResult.getString("department"),
-                                        queryResult.getString("institution"),
-                                        queryResult.getString("last_login"));
+                                        result.getString("dept_id"),
+                                        result.getString("inst_id"),
+                                        result.getString("last_login"));
                 
                 userList.add(user);
                 userIDList.put(user_id, user_id);
@@ -94,7 +92,7 @@ public class UserAccountDB {
             logger.debug("No of user account retrieved: " + userList.size());
         }
         catch (SQLException e) {
-            logger.error("SQLException while retrieving user accounts from database.");
+            logger.error("SQLException when retrieving user accounts from DB!");
             logger.error(e.getMessage());
         }
         
@@ -104,33 +102,33 @@ public class UserAccountDB {
     // Return the user account that requested this job.
     public static UserAccount getUser(int jobID) {
         UserAccount user = null;
-        String queryStr = "SELECT * FROM user_account WHERE user_id = ("
-                + "SELECT user_id FROM submitted_job WHERE job_id = "
+        String queryStr = "SELECT * FROM user_account u NATURAL JOIN dept d "
+                + "WHERE u.dept_id = d.dept_id AND u.user_id = (SELECT "
+                + "user_id FROM submitted_job WHERE job_id = "
                 + jobID + ")";
+        ResultSet result = DBHelper.runQuery(queryStr);
         
-        try (PreparedStatement queryStm = conn.prepareStatement(queryStr)) {
-            ResultSet queryResult = queryStm.executeQuery();
-           
-            if (queryResult.next()) {
-                user = new UserAccount(queryResult.getString("user_id"),
-                                       queryResult.getInt("role_id"),
-                                       queryResult.getString("first_name"),
-                                       queryResult.getString("last_name"),
-                                       queryResult.getString("email"),
-                                       queryResult.getBoolean("active"),
+        try {
+            if (result.next()) {
+                user = new UserAccount(result.getString("user_id"),
+                                       result.getInt("role_id"),
+                                       result.getString("first_name"),
+                                       result.getString("last_name"),
+                                       result.getString("email"),
+                                       result.getBoolean("active"),
                                        "password",
-                                       queryResult.getString("department"),
-                                       queryResult.getString("institution"),
-                                       queryResult.getString("last_login"));
+                                       result.getString("dept_id"),
+                                       result.getString("inst_id"),
+                                       result.getString("last_login"));
                 
             }
            
             logger.debug("For Job ID " + jobID + " User ID is " +
-                    queryResult.getString("user_id"));
+                    result.getString("user_id"));
         }
         catch (SQLException e) {
-            logger.error("SQLException encountered while retrieving user "
-                    + "account for Job ID: " + jobID);
+            logger.error("SQLException when retrieving user account for Job ID: " 
+                        + jobID);
             logger.error(e.getMessage());
         }
 
@@ -143,21 +141,19 @@ public class UserAccountDB {
                 + "SELECT user_id FROM submitted_job WHERE job_id = "
                 + jobID + ")";
         String email = null;
-       
-        try (PreparedStatement queryStm = conn.prepareStatement(queryStr))
-        {
-            ResultSet queryResult = queryStm.executeQuery();
-           
-            if (queryResult.next()) {
-                email = queryResult.getString("email");
+        ResultSet result = DBHelper.runQuery(queryStr);
+        
+        try {
+            if (result.next()) {
+                email = result.getString("email");
             }
            
             logger.debug("For Job ID " + jobID + " user email address is " +
                     email);
         }
         catch (SQLException e) {
-            logger.error("SQLException encountered while retrieving email "
-                    + "address for Job ID: " + jobID);
+            logger.error("SQLException when retrieving email address for Job ID: " 
+                        + jobID);
             logger.error(e.getMessage());
         }
        
@@ -167,14 +163,14 @@ public class UserAccountDB {
     // Check the password entered by the user, and if the password is valid, 
     // a UserAccount object will be return.
     public static UserAccount checkPwd(String user_id, String pwd) {
-        String queryStr = "SELECT * FROM user_account WHERE user_id = ?";
+        String queryStr = "SELECT * FROM user_account u NATURAL JOIN dept d "
+                + "WHERE u.dept_id = d.dept_id AND u.user_id = ?";
         String pwd_hash = null;
         UserAccount acct = null;
 
         try (PreparedStatement queryPwd = conn.prepareStatement(queryStr)) 
         {
-            // Build the query condition using the value from the parameter
-            // user_id.
+            // Build the query condition using the parameter user_id.
             queryPwd.setString(1, user_id);
             ResultSet queryResult = queryPwd.executeQuery();
             
@@ -186,7 +182,7 @@ public class UserAccountDB {
                 if (BCrypt.checkpw(pwd, pwd_hash)) {
                     logger.info(user_id + ": password valid.");
                     // Construct a UserAccount object based on the return result
-                    // BUT set the password and last_login to empty i.e. " ".
+                    // BUT set the password and last_login to default value.
                     acct = new UserAccount(queryResult.getString("user_id"),
                                         queryResult.getInt("role_id"),
                                         queryResult.getString("first_name"),
@@ -194,13 +190,13 @@ public class UserAccountDB {
                                         queryResult.getString("email"),
                                         queryResult.getBoolean("active"),
                                         "password",
-                                        queryResult.getString("department"),
-                                        queryResult.getString("institution"),
+                                        queryResult.getString("dept_id"),
+                                        queryResult.getString("inst_id"),
                                         "last-login");
                 }
             }
         } catch (SQLException e) {
-            logger.error("SQLException encountered at checkPwd.");
+            logger.error("SQLException at checkPwd.");
             logger.error(e.getMessage());
         } 
         
@@ -221,8 +217,8 @@ public class UserAccountDB {
 
         String insertStr = "INSERT INTO user_account"
                 + "(user_id, role_id, first_name, last_name, email, pwd, "
-                + "active, department, institution)" 
-                + "VALUES (?,?,?,?,?,?,?,?,?)";
+                + "active, dept_id)" 
+                + "VALUES (?,?,?,?,?,?,?,?)";
         PreparedStatement insertStm = conn.prepareStatement(insertStr);
         
         // Build the INSERT statement using the values from the current
@@ -234,8 +230,7 @@ public class UserAccountDB {
         insertStm.setString(5, newAcct.getEmail());
         insertStm.setString(6, pwd_hash);
         insertStm.setBoolean(7, newAcct.getActive());
-        insertStm.setString(8, newAcct.getDepartment());
-        insertStm.setString(9, newAcct.getInstitution());
+        insertStm.setString(8, newAcct.getDept_id());
         // Execute the INSERT statement
         insertStm.executeUpdate();
     }
@@ -276,20 +271,19 @@ public class UserAccountDB {
     
     // Update the account detail of this user.
     public static void updateAccount(UserAccount user) throws SQLException {
-        String updateStr = "UPDATE user_account SET institution = ?, "
-                           + "department = ?, first_name = ?, last_name = ?, "
+        String updateStr = "UPDATE user_account SET dept_id = ?, "
+                           + "first_name = ?, last_name = ?, "
                            + "email = ?, active = ?, role_id = ? WHERE "
                            + "user_id = ?";
         
         PreparedStatement updateStm = conn.prepareStatement(updateStr);
-        updateStm.setString(1, user.getInstitution());
-        updateStm.setString(2, user.getDepartment());
-        updateStm.setString(3, user.getFirst_name());
-        updateStm.setString(4, user.getLast_name());
-        updateStm.setString(5, user.getEmail());
-        updateStm.setBoolean(6, user.getActive());
-        updateStm.setInt(7, user.getRole_id());
-        updateStm.setString(8, user.getUser_id());
+        updateStm.setString(1, user.getDept_id());
+        updateStm.setString(2, user.getFirst_name());
+        updateStm.setString(3, user.getLast_name());
+        updateStm.setString(4, user.getEmail());
+        updateStm.setBoolean(5, user.getActive());
+        updateStm.setInt(6, user.getRole_id());
+        updateStm.setString(7, user.getUser_id());
         // Excute the UPDATE statement
         updateStm.executeUpdate();
     }
