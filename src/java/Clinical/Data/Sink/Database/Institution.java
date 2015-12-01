35f5aef987144dc5bf87b3171e3b94436695a1f1
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-12-01 09:48:06 +0800

    01Dec - Implementation for database 2.0

--- src/java/Clinical/Data/Sink/Database/Institution.java
+++ src/java/Clinical/Data/Sink/Database/Institution.java
@@ -11,24 +11,25 @@ package Clinical.Data.Sink.Database;
  * 
  * Revision History
  * 13-Nov-2015 - Created with all the standard getters and setters.
+ * 01-Dec-2015 - Implementation for database 2.0
  */
 
 public class Institution {
     // institution table fields
-    private String institution_code, institution_name;
+    private String inst_id, inst_name;
 
-    public Institution(String institution_code, String institution_name) {
-        this.institution_code = institution_code;
-        this.institution_name = institution_name;
+    public Institution(String inst_id, String inst_name) {
+        this.inst_id = inst_id;
+        this.inst_name = inst_name;
     }
 
     // Machine generated getters and setters
-    public String getInstitution_code() 
-    {   return institution_code;    }
-    public void setInstitution_code(String institution_code) 
-    {   this.institution_code = institution_code;   }
-    public String getInstitution_name() 
-    {   return institution_name;    }
-    public void setInstitution_name(String institution_name) 
-    {   this.institution_name = institution_name;   }
+    public String getInst_id() 
+    {   return inst_id;    }
+    public void setInst_id(String inst_id) 
+    {   this.inst_id = inst_id;   }
+    public String getInst_name() 
+    {   return inst_name;    }
+    public void setInst_name(String inst_name) 
+    {   this.inst_name = inst_name;   }
 }
