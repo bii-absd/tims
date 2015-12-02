d95d69ea232ff2e0f7549e53fa5216de0a806aa3
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-12-02 10:32:03 +0800

    02Dec - Streamline the createConfigFile method.

--- src/java/Clinical/Data/Sink/Bean/GEXAffymetrixBean.java
+++ src/java/Clinical/Data/Sink/Bean/GEXAffymetrixBean.java
@@ -3,14 +3,11 @@
  */
 package Clinical.Data.Sink.Bean;
 
-import Clinical.Data.Sink.Database.PipelineCommandDB;
+import Clinical.Data.Sink.Database.PipelineDB;
 import Clinical.Data.Sink.Database.SubmittedJob;
 import Clinical.Data.Sink.Database.SubmittedJobDB;
 import Clinical.Data.Sink.General.Constants;
 import Clinical.Data.Sink.General.SelectOneMenuList;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import javax.annotation.PostConstruct;
@@ -29,16 +26,16 @@ import javax.faces.bean.ViewScoped;
  * removed the abstract method allowToSubmitJob().
  * 25-Nov-2015 - Renamed pipelineType to pipelineTech. Implementation for 
  * database 2.0
+ * 02-Dec-2015 - Streamline the createConfigFile method.
  */
 
 @ManagedBean (name="gexAffymetrixBean")
 @ViewScoped
 public class GEXAffymetrixBean extends ConfigBean {
-    private String probeFilter;
 
     public GEXAffymetrixBean() {
         pipelineName = Constants.GEX_AFFYMETRIX;
-        pipelineTech = PipelineCommandDB.getPipelineTechnology(pipelineName);
+        pipelineTech = PipelineDB.getPipelineTechnology(pipelineName);
         
         logger.debug("GEXAffymetrixBean created.");
     }
@@ -48,62 +45,6 @@ public class GEXAffymetrixBean extends ConfigBean {
         init();
     }
 
-    @Override
-    public Boolean createConfigFile() {
-        Boolean result = Constants.OK;
-        String configDir = AuthenticationBean.getHomeDir() +
-                Constants.getCONFIG_PATH();
-        // configFile will be send to the pipeline during execution
-        File configFile = new File(configDir + 
-                Constants.getCONFIG_FILE_NAME() + submitTimeInFilename + 
-                Constants.getCONFIG_FILE_EXT());
-
-        pipelineConfig = configFile.getAbsolutePath();
-        logger.debug("Pipeline config file: " + pipelineConfig);
-
-        try (FileWriter fw = new FileWriter(configFile)) {
-            input = inputFile.getLocalDirectoryPath();
-            sample = sampleFile.getLocalDirectoryPath() + 
-                            sampleFile.getInputFilename();
-            // Create the config file
-            configFile.createNewFile();
-            // Write to the config file according to the format needed 
-            // by the pipeline.
-            fw.write("### INPUT parameters\n" +
-                     "STUDY_ID\t=\t" + getStudyID() + "_" + submitTimeInFilename +
-                     "\nTYPE\t=\t" + getType() +
-                     "\nINPUT_FILE\t=\t" + input +
-                     "\nCTRL_FILE\t=\t" +
-                     "\nSAMPLES_ANNOT_FILE\t=\t" + sample + "\n\n");
-
-            fw.write("### PROCESSING parameters\n" +
-                     "NORMALIZATION\t=\t" + getNormalization() +
-                     "\nPROBE_Filtering\t=\t" + probeFilter +
-                     "\nPROBE_SELECTION\t=\t" + booleanToYesNo(isProbeSelect()) +
-                     "\nPHENOTYPE_COLUMN\t=\t" + getPhenotype() + "\n\n");
-
-            fw.write("### Output file after normalization and processing\n" +
-                     "OUTPUT\t=\t" + pipelineOutput + "\n\n");
-
-            fw.write("### Further Processing\n" +
-                     "SAMPLE_AVERAGING\t=\t" + getSampleAverage() +
-                     "\nSTANDARDIZATION\t=\t" + getStdLog2Ratio() + "\n\n");
-
-            fw.write("### Report Generation\n" +
-                     "REP_FILENAME\t=\t" + pipelineReport + "\n\n");
-
-            logger.debug("Pipeline config file created at " + pipelineConfig);
-        }
-        catch (IOException e) {
-            logger.error(AuthenticationBean.getUserName() + 
-                        ": encountered error when writing pipeline config file.");
-
-            result = Constants.NOT_OK;
-        }
-        
-        return result;
-    }
-    
     @Override
     public void updateJobSubmissionStatus() {
         // Only update the jobSubmissionStatus if all the input files are 
@@ -147,16 +88,17 @@ public class GEXAffymetrixBean extends ConfigBean {
         return result;
     }
 
+    // Make the necessary setup to those attributes that are relevant to this
+    // pipeline, and then call the base class method to create the Config File.
+    public Boolean createConfigFile() {
+        input = inputFile.getLocalDirectoryPath();
+        sample = sampleFile.getLocalDirectoryPath() + sampleFile.getInputFilename();
+        // Call the base class method to create the Config File.        
+        return super.createConfigFile();
+    }
+    
     // Return the list of Affymetrix type.
     public LinkedHashMap<String,String> getTypeList() {
         return SelectOneMenuList.getAffymetrixTypeList();
-    }
-    
-    // Machine generated getters and setters
-    public String getProbeFilter() {
-        return probeFilter;
-    }
-    public void setProbeFilter(String probeFilter) {
-        this.probeFilter = probeFilter;
     }
 }
