48e9b6a6a7c0f8cdd9b1eead91ad080ef495b8d7
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-12-03 17:55:33 +0800

    03Dec - Added in one new attribute annot_ver.

--- src/java/Clinical/Data/Sink/Database/FinalizedOutput.java
+++ src/java/Clinical/Data/Sink/Database/FinalizedOutput.java
@@ -12,16 +12,32 @@ package Clinical.Data.Sink.Database;
  * 
  * Revision History
  * 02-Dec-2015 - Created with all the standard getters and setters.
+ * 03-Dec-2015 - Added in one new attribute annot_ver.
  */
 
 public class FinalizedOutput {
-    String subject_id;
+    String annot_ver, subject_id;
     int array_index, job_id;
 
     // Machine generated code
-    public FinalizedOutput(String subject_id, int job_id) {
+    public FinalizedOutput(int array_index, String annot_ver, String subject_id, 
+            int job_id) {
+        this.array_index = array_index;
+        this.annot_ver = annot_ver;
         this.subject_id = subject_id;
         this.job_id = job_id;
+    }
+    public int getArray_index() {
+        return array_index;
+    }
+    public void setArray_index(int array_index) {
+        this.array_index = array_index;
+    }
+    public String getAnnot_ver() {
+        return annot_ver;
+    }
+    public void setAnnot_ver(String annot_ver) {
+        this.annot_ver = annot_ver;
     }
     public String getSubject_id() {
         return subject_id;
