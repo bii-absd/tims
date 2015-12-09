588a47cc98c95d880be31d5d52b09433f60b978b
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-12-09 17:20:30 +0800

    09Dec - Added in the module for adding and updating department info.

--- src/java/Clinical/Data/Sink/Database/DepartmentDB.java
+++ src/java/Clinical/Data/Sink/Database/DepartmentDB.java
@@ -3,6 +3,7 @@
  */
 package Clinical.Data.Sink.Database;
 
+import Clinical.Data.Sink.General.Constants;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -24,12 +25,13 @@ import org.apache.logging.log4j.LogManager;
  * 
  * Revision History
  * 13-Nov-2015 - Created with all the standard getters and setters.
- * 16-Nov-2015 - Added 2 new methods, getAllDeptList() to get the full 
+ * 16-Nov-2015 - Added 2 new methods, getDeptList() to get the full 
  * department list and getDeptHashMap(inst_code) to get the department list 
  * for the respective institution.
  * 30-Nov-2015 - Implementation for database 2.0
  * 09-Dec-2015 - Added new method getDeptIDList, to return the list of dept_id 
- * setup under the specific institution.
+ * setup under the specific institution. Added in the module for adding and
+ * updating department info.
  */
 
 public class DepartmentDB implements Serializable {
@@ -37,17 +39,22 @@ public class DepartmentDB implements Serializable {
     private final static Logger logger = LogManager.
             getLogger(DepartmentDB.class.getName());
     private final static Connection conn = DBHelper.getDBConn();
-    private final static List<Department> AllDeptList = new ArrayList<>();
+    private final static List<Department> deptList = new ArrayList<>();
 
     public DepartmentDB() {}
     
+    // Clear the department list, so that it will be rebuild again.
+    private static void clearDeptList() {
+        deptList.clear();
+    }
+    
     // Return the full list of Department setup in the system.
-    public static List<Department> getAllDeptList() {
+    public static List<Department> getDeptList() {
         // Only execute the query if the list is empty.
         // To prevent the query from being run multiple times.
-        if (AllDeptList.isEmpty()) {
+        if (deptList.isEmpty()) {
             ResultSet result = DBHelper.runQuery
-                ("SELECT * FROM dept ORDER BY inst_id");
+                ("SELECT * FROM dept ORDER BY dept_id");
             
             try {
                 while (result.next()) {
@@ -55,7 +62,7 @@ public class DepartmentDB implements Serializable {
                                         result.getString("inst_id"),
                                         result.getString("dept_id"),
                                         result.getString("dept_name"));
-                    AllDeptList.add(dept);
+                    deptList.add(dept);
                 }
             }
             catch (SQLException e) {
@@ -64,7 +71,59 @@ public class DepartmentDB implements Serializable {
             }
         }
         
-        return AllDeptList;
+        return deptList;
+    }
+    
+    // Insert the new department ID into database.
+    public static Boolean insertDepartment(Department dept) {
+        Boolean result = Constants.OK;
+        String insertStr = "INSERT INTO dept(dept_id,inst_id,dept_name) VALUES(?,?,?)";
+        
+        try (PreparedStatement insertStm = conn.prepareStatement(insertStr)) {
+            insertStm.setString(1, dept.getDept_id());
+            insertStm.setString(2, dept.getInst_id());
+            insertStm.setString(3, dept.getDept_name());
+            insertStm.executeUpdate();
+            // Clear and rebuild the department list.
+            clearDeptList();
+            getDeptList();
+            logger.debug("New department ID inserted into database: " + 
+                    dept.getDept_id());
+        }
+        catch (SQLException e) {
+            logger.error("SQLException when inserting department ID!");
+            logger.error(e.getMessage());
+            result = Constants.NOT_OK;
+        }
+        
+        return result;
+    }
+    
+    // Update the department information in the database.
+    public static Boolean updateDepartment(Department dept) {
+        Boolean result = Constants.OK;
+        String updateStr = "UPDATE dept SET inst_id = ?, dept_name = ? "
+                + "WHERE dept_id = ?";
+        
+        try (PreparedStatement updateStm = conn.prepareStatement(updateStr)) {
+            updateStm.setString(1, dept.getInst_id());
+            updateStm.setString(2, dept.getDept_name());
+            updateStm.setString(3, dept.getDept_id());
+            
+            updateStm.executeUpdate();
+            // Clear and rebuild the department list.
+            clearDeptList();
+            getDeptList();
+            logger.debug("Department " + dept.getDept_id() + " updated.");
+        }
+        catch (SQLException e) {
+            logger.error("SQLException when updating department: " + 
+                    dept.getDept_id());
+            logger.error(e.getMessage());
+            result = Constants.NOT_OK;
+        }
+        
+        return result;
     }
     
     // Return the list of dept_id setup under this inst_id.
