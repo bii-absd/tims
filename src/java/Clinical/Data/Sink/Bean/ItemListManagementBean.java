58125a72ac6d8fa04b2260b10cceb2bce035f674
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-12-09 17:21:00 +0800

    09Dec - Added in the module for adding and updating department info.

--- src/java/Clinical/Data/Sink/Bean/ItemListManagementBean.java
+++ src/java/Clinical/Data/Sink/Bean/ItemListManagementBean.java
@@ -5,10 +5,12 @@ package Clinical.Data.Sink.Bean;
 
 // Libraries for Log4j
 import Clinical.Data.Sink.Database.Department;
+import Clinical.Data.Sink.Database.DepartmentDB;
 import Clinical.Data.Sink.Database.Institution;
 import Clinical.Data.Sink.Database.InstitutionDB;
 import Clinical.Data.Sink.General.Constants;
 import java.io.Serializable;
+import java.util.LinkedHashMap;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -31,6 +33,7 @@ import org.apache.logging.log4j.LogManager;
  * method createNewInstitution() for creating new institution.
  * 18-Nov-2015 - Added one new method onInstRowEdit() to allow user to edit
  * the institution's information.
+ * 09-Dec-2015 - Added in the module for adding and updating department info.
  */
 
 @ManagedBean (name="itemListMgntBean")
@@ -40,9 +43,9 @@ public class ItemListManagementBean implements Serializable {
     private final static Logger logger = LogManager.
             getLogger(ItemListManagementBean.class.getName());
     // Attributes for Institution object
-    private String inst_code, inst_name;
+    private String inst_id, inst_name;
     // Attributes for Department object
-    private String dept_code, dept_name;
+    private String dept_id, dept_name;
     private List<Institution> instList;
     private List<Department> deptList;
     
@@ -54,26 +57,40 @@ public class ItemListManagementBean implements Serializable {
     
     @PostConstruct
     public void init() {
+        System.out.println("Post Construct.");
         instList = InstitutionDB.getInstList();
+        deptList = DepartmentDB.getDeptList();
     }
 
-    // Create the new institution code.
-    public String createNewInstitution() {
-        FacesContext fc = getFacesContext();
-        Institution newInst = new Institution(inst_code, inst_name);
+    // Create the new institution ID
+    public String createNewInstID() {
+        Institution newInst = new Institution(inst_id, inst_name);
         
         if (InstitutionDB.insertInstitution(newInst)) {
             logger.info(AuthenticationBean.getUserName() +
-                    ": created new institution code: " + inst_code);
-            fc.addMessage(null, new FacesMessage(
-                    FacesMessage.SEVERITY_INFO,
-                    "New institution code created.", ""));
+                    ": created new institution ID: " + inst_id);
+            addFacesInfoMsg("New institution ID created.");
         }
         else {
-            logger.error("Failed to create new institution code: " + inst_code);
-            fc.addMessage(null, new FacesMessage(
-                    FacesMessage.SEVERITY_ERROR,
-                    "Failed to create new institution code!", ""));
+            logger.error("Failed to create new institution ID: " + inst_id);
+            addFacesErrorMsg("Failed to create new institution ID!");
+        }
+        
+        return Constants.ITEM_LIST_MANAGEMENT;
+    }
+    
+    // Create the new department ID
+    public String createNewDeptID() {
+        Department newDept = new Department(inst_id, dept_id, dept_name);
+        
+        if (DepartmentDB.insertDepartment(newDept)) {
+            logger.info(AuthenticationBean.getUserName() +
+                    ": created new department ID: " + dept_id);
+            addFacesInfoMsg("New department ID created.");
+        }
+        else {
+            logger.error("Failed to create new department ID: " + dept_id);
+            addFacesErrorMsg("Failed to create new department ID!");
         }
         
         return Constants.ITEM_LIST_MANAGEMENT;
@@ -81,27 +98,44 @@ public class ItemListManagementBean implements Serializable {
     
     // Update the institution table in the database.
     public void onInstRowEdit(RowEditEvent event) {
-        FacesContext fc = getFacesContext();
-        
         if (InstitutionDB.updateInstitution((Institution) event.getObject())) {
             logger.info(AuthenticationBean.getUserName() + 
                     ": updated Institution.");
-            fc.addMessage(null, new FacesMessage(
-                    FacesMessage.SEVERITY_INFO,
-                    "Institution updated.", ""));
+            addFacesInfoMsg("Institution updated.");
         }
         else {
             logger.error("Institution update failed.");
-            fc.addMessage(null, new FacesMessage(
-                    FacesMessage.SEVERITY_ERROR,
-                    "Failed to update institution!", ""));
+            addFacesErrorMsg("Failed to update institution!");
         }
+    }
+    
+    // Return the HashMap of inst_name-inst_id for user selection.
+    public LinkedHashMap<String, String> getInstNameHash() {
+        return InstitutionDB.getInstNameHash();
     }
     
     // Update the department table in the database.
     public void onDeptRowEdit(RowEditEvent event) {
-        logger.info(AuthenticationBean.getUserName() + 
-                ": updated Department.");
+        if (DepartmentDB.updateDepartment((Department) event.getObject())) {
+            logger.info(AuthenticationBean.getUserName() +
+                    ": updated Department.");
+            addFacesInfoMsg("Department updated.");
+        }
+        else {
+            logger.error("Department update failed.");
+            addFacesErrorMsg("Failed to update department!");
+        }
+    }
+    
+    // Show a Faces Info Message at the current context.
+    private void addFacesInfoMsg(String msg) {
+        getFacesContext().addMessage(null, 
+                new FacesMessage(FacesMessage.SEVERITY_INFO, msg, ""));
+    }
+    // Show a Faces Error Message ath the current context.
+    private void addFacesErrorMsg(String msg) {
+        getFacesContext().addMessage(null, 
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, ""));
     }
     
     // Retrieve the faces context
@@ -116,11 +150,17 @@ public class ItemListManagementBean implements Serializable {
     public void setInstList(List<Institution> instList) {
         this.instList = instList;
     }
-    public String getInst_code() {
-        return inst_code;
+    public List<Department> getDeptList() {
+        return deptList;
     }
-    public void setInst_code(String inst_code) {
-        this.inst_code = inst_code;
+    public void setDeptList(List<Department> deptList) {
+        this.deptList = deptList;
+    }
+    public String getInst_id() {
+        return inst_id;
+    }
+    public void setInst_id(String inst_id) {
+        this.inst_id = inst_id;
     }
     public String getInst_name() {
         return inst_name;
@@ -128,11 +168,11 @@ public class ItemListManagementBean implements Serializable {
     public void setInst_name(String inst_name) {
         this.inst_name = inst_name;
     }
-    public String getDept_code() {
-        return dept_code;
+    public String getDept_id() {
+        return dept_id;
     }
-    public void setDept_code(String dept_code) {
-        this.dept_code = dept_code;
+    public void setDept_id(String dept_id) {
+        this.dept_id = dept_id;
     }
     public String getDept_name() {
         return dept_name;
