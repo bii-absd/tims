f1191e5e6ad4945b9d6a4cfa2a5e6371cdc00eeb
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-12-11 12:27:22 +0800

    11Dec - Changed to abstract class. Added 4 methods, updateStudy, getStudyList, queryStudy and clearStudyList().

--- src/java/Clinical/Data/Sink/Database/StudyDB.java
+++ src/java/Clinical/Data/Sink/Database/StudyDB.java
@@ -8,14 +8,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.sql.Date;
 // Libraries for Log4j
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 /**
- * StudyDB is not mean to be instantiate, its main job is to perform
- * SQL operations on the study table in the database.
+ * StudyDB is an abstract class and not mean to be instantiate, its main job 
+ * is to perform SQL operations on the study table in the database.
  * 
  * Author: Tay Wei Hong
  * Date: 07-Dec-2015
@@ -23,31 +26,36 @@ import org.apache.logging.log4j.LogManager;
  * Revision History
  * 07-Dec-2015 - First baseline with two static methods, insertStudy and 
  * getAnnotHashMap.
+ * 11-Dec-2015 - Changed to abstract class. Added 4 methods, updateStudy, 
+ * getStudyList, queryStudy and clearStudyList().
  */
 
-public class StudyDB {
+public abstract class StudyDB {
     // Get the logger for Log4j
     private final static Logger logger = LogManager.
             getLogger(StudyDB.class.getName());
     private final static Connection conn = DBHelper.getDBConn();
+    private static List<Study> studyList = new ArrayList<>();
 
-    public StudyDB() {}
-    
     // Insert the new study into database
     public static Boolean insertStudy(Study study) {
         Boolean result = Constants.OK;
-        String insertStr = "INSERT INTO study(study_id,user_id,annot_ver,"
-                         + "description,date,completed) VALUES(?,?,?,?,?,?)";
+        String insertStr = "INSERT INTO study(study_id,dept_id,user_id,"
+                         + "annot_ver,description,date,completed) "
+                         + "VALUES(?,?,?,?,?,?,?)";
         
         try (PreparedStatement insertStm = conn.prepareStatement(insertStr)) {
             insertStm.setString(1, study.getStudy_id());
-            insertStm.setString(2, study.getUser_id());
-            insertStm.setString(3, study.getAnnot_ver());
-            insertStm.setString(4, study.getDescription());
-            insertStm.setDate(5, study.getSqlDate());
-            insertStm.setBoolean(6, study.getCompleted());
+            insertStm.setString(2, study.getDept_id());
+            insertStm.setString(3, study.getUser_id());
+            insertStm.setString(4, study.getAnnot_ver());
+            insertStm.setString(5, study.getDescription());
+            insertStm.setDate(6, study.getSqlDate());
+            insertStm.setBoolean(7, study.getCompleted());
+
             insertStm.executeUpdate();
-            
+            // Clear the study list, so that it will be rebuild again.
+            clearStudyList();
             logger.debug("New Study ID inserted into database: " + 
                     study.getStudy_id());
         }
@@ -56,6 +64,31 @@ public class StudyDB {
             logger.error(e.getMessage());
             result = Constants.NOT_OK;
         }
+        return result;
+    }
+    
+    // Update the study information in the database
+    public static Boolean updateStudy(Study study) {
+        Boolean result = Constants.OK;
+        String updateStr = "UPDATE study SET dept_id = ?, description = ?, "
+                         + "date = ? WHERE study_id = ?";
+        
+        try (PreparedStatement updateStm = conn.prepareStatement(updateStr)) {
+            updateStm.setString(1, study.getDept_id());
+            updateStm.setString(2, study.getDescription());
+            updateStm.setDate(3, study.getSqlDate());
+            updateStm.setString(4, study.getStudy_id());
+            
+            updateStm.executeUpdate();
+            logger.debug("Updated study: " + study.getStudy_id());
+        }
+        catch (SQLException e) {
+            logger.error("SQLException when updating study: " + 
+                    study.getStudy_id());
+            logger.error(e.getMessage());
+            result = Constants.NOT_OK;
+        }
+        
         return result;
     }
     
@@ -76,5 +109,67 @@ public class StudyDB {
         }
         
         return annotList;
+    }
+    
+    // Return the list of Study ID setup for the department that this
+    // user ID belongs to.
+    public static LinkedHashMap<String, String> getStudyList(String userID) {
+        LinkedHashMap<String, String> studyList = new LinkedHashMap<>();
+        String queryStr = "SELECT study_id FROM study WHERE dept_id = "
+                        + "(SELECT dept_id FROM user_account WHERE user_id = ?)";
+        
+        try (PreparedStatement queryStm = conn.prepareStatement(queryStr)) {
+            queryStm.setString(1, userID);
+            ResultSet rs = queryStm.executeQuery();
+            
+            while (rs.next()) {
+                studyList.put(rs.getString("study_id"), rs.getString("study_id"));
+            }
+            logger.debug("Study list for " + userID + "'s department retrieved.");
+        }
+        catch (SQLException e) {
+            logger.error("SQLException when query study!");
+            logger.error(e.getMessage());
+        }
+        return studyList;
+    }
+    
+    // Return the list of Study ID setup in the system.
+    // Note: Users will not have access to create Study ID i.e. only the 
+    // administrator do.
+    public static List<Study> queryStudy() {
+        // Only execute the query if the list is empty.
+        if (studyList.isEmpty()) {
+            ResultSet rs = DBHelper.runQuery("SELECT * FROM study ORDER BY date DESC");
+            
+            try {
+                while (rs.next()) {
+                    Study tmp = new Study(
+                            rs.getString("study_id"),
+                            rs.getString("dept_id"),
+                            rs.getString("user_id"),
+                            rs.getString("annot_ver"),
+                            rs.getString("description"),
+                            rs.getDate("date"),
+                            rs.getBoolean("completed"));
+                    
+                    studyList.add(tmp);
+                }
+                logger.debug("Query study completed.");
+            }
+            catch (SQLException e) {
+                logger.error("SQLException when query study!");
+                logger.error(e.getMessage());
+                // Exception has occurred, return back a empty list.
+                return new ArrayList<>();
+            }
+        }
+        
+        return studyList;
+    }
+    
+    // Clear the study list, so that the query to the database get to run again.
+    public static void clearStudyList() {
+        studyList.clear();
     }
 }
