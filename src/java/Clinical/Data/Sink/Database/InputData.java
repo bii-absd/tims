24fd4bc6f7e30d8115be0d849c24c5e7b5b52a98
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-12-16 15:42:45 +0800

    16Dec - Initial commit after creation.

---
+++ src/java/Clinical/Data/Sink/Database/InputData.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright @2015
+ */
+package Clinical.Data.Sink.Database;
+
+/**
+ * InputData is used to represent the input_data table in the database.
+ * 
+ * Author: Tay Wei Hong
+ * Date: 16-Dec-2015
+ * 
+ * Revision History
+ * 16-Dec-2015 - Created with all the standard getters and setters.
+ */
+
+public class InputData {
+    // input_data table attributes
+    private String study_id, filename, filepath, description, date;
+    private int sn;
+
+    public InputData(String study_id, String filename, String filepath, 
+            String description, int sn, String date) {
+        this.study_id = study_id;
+        this.filename = filename;
+        this.filepath = filepath;
+        this.description = description;
+        this.sn = sn;
+        this.date = date;
+    }
+    
+    // Machine generated getters and setters.
+    public String getStudy_id() {
+        return study_id;
+    }
+    public void setStudy_id(String study_id) {
+        this.study_id = study_id;
+    }
+    public String getFilename() {
+        return filename;
+    }
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+    public String getFilepath() {
+        return filepath;
+    }
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getSn() {
+        return sn;
+    }
+    public void setSn(int sn) {
+        this.sn = sn;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
