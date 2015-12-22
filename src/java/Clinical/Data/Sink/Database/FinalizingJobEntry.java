335ad8fbcb900108dbe6c5351f5de7cfdf974e0e
Tay Wei Hong <whtay75@yahoo.com.sg>
2015-12-22 15:28:23 +0800

    22Dec - Initial commit after creation.

---
+++ src/java/Clinical/Data/Sink/Database/FinalizingJobEntry.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright @2015
+ */
+package Clinical.Data.Sink.Database;
+
+/**
+ * FinalizingJobEntry is used to represent the job_id, pipeline technology 
+ * (i.e. tid), pipeline name and submission time that will be retrieved from
+ * database when the user is ready to finalize the study.
+ * 
+ * Author: Tay Wei Hong
+ * Date: 22-Dec-2015
+ * 
+ * Revision History
+ * 22-Dec-2015 - Created with the necessary methods implemented.
+ */
+
+public class FinalizingJobEntry {
+    private int job_id;
+    private String tid, pipeline_name, submit_time;
+
+    public FinalizingJobEntry(int job_id, String tid, String pipeline_name, 
+            String submit_time) {
+        this.job_id = job_id;
+        this.tid = tid;
+        this.pipeline_name = pipeline_name;
+        this.submit_time = submit_time;
+    }
+
+    // Machine generated getters and setters.
+    public int getJob_id() 
+    { return job_id; }
+    public void setJob_id(int job_id) 
+    { this.job_id = job_id; }
+    public String getTid() 
+    { return tid; }
+    public void setTid(String tid) 
+    { this.tid = tid; }
+    public String getPipeline_name() 
+    { return pipeline_name; }
+    public void setPipeline_name(String pipeline_name) 
+    { this.pipeline_name = pipeline_name; }
+    public String getSubmit_time() 
+    { return submit_time; }
+    public void setSubmit_time(String submit_time) 
+    { this.submit_time = submit_time; }
+}
